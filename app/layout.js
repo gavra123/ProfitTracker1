66cd44804fcf2034fc06b6a5db06d928e0ce68c5
gavra123 <mgavric147@gmail.com>
2026-06-06 18:24:01 +0200

    Create RootLayout component with metadata for Profit Tracker

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Profit Tracker',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="sr">
+      <body>{children}</body>
+    </html>
+  )
+}
